d2067cd6d102bd62255093ee1d62083603da616c
Anand Chowdhary <mail@anandchowdhary.com>
2020-09-01 22:27:53 +0530

    :wrench: Add now.json for env

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,31 @@
 import { NowRequest, NowResponse } from "@now/node";
+import axios from "axios";
+
+const allowedRepos = ["AnandChowdhary/life"];
+
+const forward = async (req: NowRequest) => {
+  const endpoint = req.query.endpoint;
+  if (!endpoint) throw new Error("Specify an endpoint");
+  let allowed = false;
+  if (typeof endpoint === "string")
+    allowedRepos.forEach(
+      (repo) => (allowed = allowed || endpoint.startsWith(`/repos/${repo}/`))
+    );
+  if (!allowed) throw new Error("Access to this repository is not allowed");
+  console.log(process.env.GITHUB_PERSONAL_ACCESS_TOKEN);
+  return (
+    await axios.get(`https://api.github.com${endpoint}`, {
+      headers: {
+        Authorization: `token ${process.env.GITHUB_PERSONAL_ACCESS_TOKEN}`,
+      },
+    })
+  ).data;
+};
 
 export default async (req: NowRequest, res: NowResponse) => {
-  return res.json({ ok: true });
+  try {
+    res.json(await forward(req));
+  } catch (error) {
+    res.status(400).json({ error: error.toString() });
+  }
 };
